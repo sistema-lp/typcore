d16700cee1145e158d22f49fd2af8c44e04a1295
kbssanova <python@lp.net.br>
2025-12-22 11:36:49 -0300

    Registrando Clientes e Dominios no Admin

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
+from .models import Client, Domain
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ('schema_name', 'name')
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'tenant')
