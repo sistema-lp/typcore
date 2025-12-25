b12795c0404f6c6bcf1afa02910de02b44912bd9
kbssanova <python@lp.net.br>
2025-12-25 09:11:25 -0300

    Migrações de produtos e customers prontas

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Product(models.Model):
+    # Deixe o Django criar o ID sozinho no banco novo
     name = models.CharField(max_length=255, verbose_name="Nome do Produto")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
     description = models.TextField(blank=True, null=True, verbose_name="Descrição")
 
     class Meta:
         app_label = 'products'
-        db_table = 'products_inventory' # <--- Mude para este nome
+        db_table = 'products_product' 
 
     def __str__(self):
         return self.name
